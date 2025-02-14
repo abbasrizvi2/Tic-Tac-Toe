07a1f3e27b536201b88cc77f1996ef114b659c57
abbasrizvi2 <abbasali0974@gmail.com>
2025-02-14 23:12:27 +0530

    TO-DO done with making components

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -7,4 +8,16 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+=======
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import App from './components/App'
+// import Practice from './components/Practice'
+import {Practice2} from './components/Practice2'
+
+createRoot(document.getElementById('root')).render(
+
+    <Practice2/>
+>>>>>>> 18cf99d (TO-DO done with making components)
 )
